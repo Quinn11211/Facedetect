b2357141b8b61aee74b52a9d191e778e00e144bb
Quinn11211 <34282038+Quinn11211@users.noreply.github.com>
2018-01-29 16:53:05 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,30 +1,17 @@
-package com.example.qpm1.faceapp1;
+package com.example.qpm1.myapplication;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.MediaScannerConnection;
 import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -37,13 +24,20 @@ import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 
-import static android.R.attr.data;
-import static com.example.qpm1.faceapp1.R.id.image;
+import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity implements View. OnClickListener {
+import static com.example.qpm1.myapplication.R.id.image;
+
+public class MainActivity extends AppCompatActivity{
     ImageView imageView1, imageView2;
-    Button btn;
 
     private Button btn;
     private ImageView imageview;
@@ -56,12 +50,6 @@ public class MainActivity extends AppCompatActivity implements View. OnClickList
         setContentView(R.layout.activity_main);
         System.loadLibrary("opencv_java3");
 
-        Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.pen);
-        imageView1 = (ImageView) findViewById(R.id.imageView);
-        imageView2 = (ImageView) findViewById(R.id.imageView2);
-
-        btn = (Button) findViewById(R.id.button);
-        imageView1.setImageBitmap(img);
 
         btn = (Button) findViewById(R.id.btn);
         imageview = (ImageView) findViewById(R.id.iv);
@@ -100,38 +88,38 @@ public class MainActivity extends AppCompatActivity implements View. OnClickList
         startActivityForResult(galleryIntent, GALLERY);
     }
 
-    private String startActivityForResult(int requestCode, int resultCode, Intent data) throws IOException {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == this.RESULT_CANCELED) {
-            return "Please try again";
+        if (resultCode == RESULT_CANCELED) {
+            return;
         }
         if (requestCode == GALLERY) {
             if (data != null) {
                 Uri contentURI = data.getData();
                 try {
                     Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    imageview.setImageBitmap(bitmap);
                     long startTime = System.nanoTime();
-                    //    CascadeClassifier faceDetector = new CascadeClassifier("D:\\opencv\\sources\\data\\lbpcascades\\lbpcascade_frontalface.xml");
+
                     CascadeClassifier faceDetector = new CascadeClassifier("D:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt.xml");
-//       CascadeClassifier faceDetector = new CascadeClassifier("D:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml");
 
-                    Bitmap bmp32 = JPGtoRGB888(bitmap);
-                    Mat sourceImage = new Mat();
-                    Utils.bitmapToMat(bmp32, sourceImage);
+                    Mat mat = new Mat();
+                    Bitmap bmp32 = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                    Utils.bitmapToMat(bmp32, mat);
 
 
-                    float size = Math.round(image.height() * 0.1f);
                     Rect rectCrop = null;
                     MatOfRect faceDetections = new MatOfRect();
                     faceDetector.detectMultiScale(image, faceDetections);
                     faceDetector.detectMultiScale(image, faceDetections, 1.1, 3, 0, new Size(90, 90), new Size(400, 400));
 
-                    if (faceDetections.toArray().length <= 1) ;
-                    return "Not a face";
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
                 }
             }
-        }
-    }
-}
+        }}}
 
 
